6e2a7d80c4f04b67d4360505fbd66286bc8c9ab4
Peter Agayby <peter.michel-agayby@uni-ulm.de>
2019-12-12 00:38:29 +0200

    Adding the winning portals for the combat and parkour scenes

---
+++ titanfall/Assets/Scripts/ParkourWinPortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourWinPortal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player")){
+            UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+        }
+    }
+}
